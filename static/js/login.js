3b53d3506fa655e3ea47e863ae9605e861ea9f86
lanyado <lanyado98@gmail.com>
2020-05-04 18:52:05 +0200

    fixed front end syntax using linters

--- static/js/login.js
+++ static/js/login.js
@@ -1,32 +1,31 @@
-function login(){
-    const box = $('.field-password')[0];
-    const password = $('#password-input').val();
+function login() {
+	const box = $('.field-password')[0];
+	const password = $('#password-input').val();
 
-    if (password){
-        $.post('/login',{
-        password: password
-        },function(response){
-            if (response.auth){
-                    window.location.href = window.location.href; //response.redirect_url
-            }
-            else{
-                box.style.animation = "shake 0.5s ease";
-                document.getElementById("modal").style.backgroundColor = 'red';
-            }
-        });
-    }
-    // get rid of the animation
-    box.addEventListener("animationend", () => {
-        box.style.animation = "";
-    });
+	if (password) {
+		$.post('/login', {
+			password,
+		}, (response) => {
+			if (response.auth) {
+				// eslint-disable-next-line no-self-assign
+				window.location.href = window.location.href; // response.redirect_url
+			} else {
+				box.style.animation = 'shake 0.5s ease';
+				document.getElementById('modal').style.backgroundColor = 'red';
+			}
+		});
+	}
+	// get rid of the animation
+	box.addEventListener('animationend', () => {
+		box.style.animation = '';
+	});
 }
 
 // let the user to login on enter key press
-$('input').on("keyup", (e) => {
-    if(e.keyCode === 13)
-       login();
+$('input').on('keyup', (e) => {
+	if (e.keyCode === 13) login();
 });
 
-$('.fa-arrow-left').on("click", () => {
-  login();
+$('.fa-arrow-left').on('click', () => {
+	login();
 });
